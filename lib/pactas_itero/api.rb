8f51ae708eb12cab8e400a96c5d12ef0ead113f3
Thorsten Eggert <t.eggert@bbtel.de>
2021-07-05 17:06:35 +0200

    add plans and plan_variants

--- lib/pactas_itero/api.rb
+++ lib/pactas_itero/api.rb
@@ -4,6 +4,8 @@ require 'pactas_itero/api/rated_items'
 require 'pactas_itero/api/customers'
 require 'pactas_itero/api/contracts'
 require 'pactas_itero/api/orders'
+require 'pactas_itero/api/plans'
+require 'pactas_itero/api/plan_variants'
 require 'pactas_itero/api/invoices'
 require "pactas_itero/api/payment_transactions"
 
@@ -14,6 +16,8 @@ module PactasItero
     include PactasItero::Api::Customers
     include PactasItero::Api::Contracts
     include PactasItero::Api::Orders
+    include PactasItero::Api::Plans
+    include PactasItero::Api::PlanVariants
     include PactasItero::Api::Invoices
     include PactasItero::Api::PaymentTransactions
   end
