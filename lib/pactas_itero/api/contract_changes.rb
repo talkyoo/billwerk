7b8d05b3dacdcdace6a86a8acdb434ab5a6f6003
Thorsten Eggert <t.eggert@bbtel.de>
2021-12-06 13:44:35 +0100

    fix contract_changes

--- lib/pactas_itero/api/contract_changes.rb
+++ lib/pactas_itero/api/contract_changes.rb
@@ -2,7 +2,7 @@ module PactasItero
   module Api
     module ContractChanges
       def contract_changes(contract_id, options = {})
-        options = options.camelize_keys
+        options = options.camelize_keys.merge( {contractId: contract_id})
         get "api/v1/contractChanges", options
       end
     end
